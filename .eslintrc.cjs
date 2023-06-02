e946043d65930c54ce9e659feb13b7a3b01cfde9
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-06-02 10:33:32 +0300

    Update rules to disable prop types and more
    Make no unused vars rule ignore vars starting with _

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,15 @@ module.exports = {
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error'
+    'jest/valid-expect': 'error',
+    'react/prop-types': 'off',
+    'no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }
+    ]
   }
 };
