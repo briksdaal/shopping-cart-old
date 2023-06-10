f5f2803a327b130055e4b43a886ba27962cd1c1f
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-06-10 03:35:04 +0300

    Create data object for all products

---
+++ src/data.js
@@ -0,0 +1,168 @@
+import chronoTriggerThumb from '../media/chrono_trigger_thumbnail.webp';
+import donkeyKongCountryThumb from '../media/donkey_kong_country_thumbnail.webp';
+import earthBoundThumb from '../media/earthbound_thumbnail.webp';
+import kirbySuperStarThumb from '../media/kirby_super_star_thumbnail.webp';
+import linkToThePastThumb from '../media/link_to_the_past_thumbnail.webp';
+import megaManXThumb from '../media/mega_man_x_thumbnail.webp';
+import secretOfManaThumb from '../media/secret_of_mana_thumbnail.webp';
+import starFoxThumb from '../media/star_fox_thumbnail.webp';
+import streetFighterIIThumb from '../media/street_fighter_ii_thumbnail.webp';
+import superBombermanThumb from '../media/super_bomberman_thumbnail.webp';
+import superMarioKartThumb from '../media/super_mario_kart_thumbnail.webp';
+import superMetroidThumb from '../media/super_metroid_thumbnail.webp';
+import superMarioWorldThumb from '../media/super_mario_world_thumbnail.webp';
+import defaultGameThumb from '../media/default_game_thumbnail.webp';
+import chronoTriggerFull from '../media/chrono_trigger_full.webp';
+import donkeyKongCountryFull from '../media/donkey_kong_country_full.webp';
+import earthBoundFull from '../media/earthbound_full.webp';
+import kirbySuperStarFull from '../media/kirby_super_star_full.webp';
+import linkToThePastFull from '../media/link_to_the_past_full.webp';
+import megaManXFull from '../media/mega_man_x_full.webp';
+import secretOfManaFull from '../media/secret_of_mana_full.webp';
+import starFoxFull from '../media/star_fox_full.webp';
+import streetFighterIIFull from '../media/street_fighter_ii_full.webp';
+import superBombermanFull from '../media/super_bomberman_full.webp';
+import superMarioKartFull from '../media/super_mario_kart_full.webp';
+import superMetroidFull from '../media/super_metroid_full.webp';
+import superMarioWorldFull from '../media/super_mario_world_full.webp';
+import defaultGameFull from '../media/default_game_full.webp';
+
+const defaultGame = {
+  imgThumbSrc: defaultGameThumb,
+  imgFullSrc: defaultGameFull
+};
+
+const products = [
+  {
+    id: 54689012,
+    title: 'Chrono Trigger',
+    price: '$34.99',
+    priceReal: 34.99,
+    description:
+      'Embark on a time-traveling RPG adventure with Chrono Trigger. Join a group of memorable characters as they navigate through different eras, facing epic challenges and unraveling a captivating storyline. Experience innovative gameplay mechanics and strategic battles in this timeless classic that has captivated gamers for decades.',
+    imgThumbSrc: chronoTriggerThumb,
+    imgFullSrc: chronoTriggerFull
+  },
+  {
+    id: 23057149,
+    title: 'Donkey Kong Country',
+    price: '$39.99',
+    priceReal: 39.99,
+    description:
+      'Join Donkey Kong and his sidekick Diddy Kong in a revitalized platforming adventure. Traverse lush jungles, challenging environments, and face off against powerful enemies in this visually stunning game. With pre-rendered 3D graphics, tight controls, and exciting levels, Donkey Kong Country offers a nostalgic gaming experience that will keep you hooked for hours.',
+    imgThumbSrc: donkeyKongCountryThumb,
+    imgFullSrc: donkeyKongCountryFull
+  },
+  {
+    id: 98765432,
+    title: 'earthBound',
+    price: '$27.99',
+    priceReal: 27.99,
+    description:
+      'Step into the quirky and offbeat world of earthBound, a unique RPG with a modern-day setting. Follow a group of unlikely heroes as they embark on a heartfelt journey to save the world from an otherworldly threat. With its distinctive sense of humor, charming characters, and emotional storytelling, earthBound is an unforgettable adventure that will leave you with a lasting impression.',
+    imgThumbSrc: earthBoundThumb,
+    imgFullSrc: earthBoundFull
+  },
+  {
+    id: 12345678,
+    title: 'Kirby Super Star',
+    price: '$29.99',
+    priceReal: 29.99,
+    description:
+      "Get ready for a colorful platforming extravaganza with Kirby Super Star. Play as the lovable and versatile Kirby as he embarks on multiple adventures, each filled with unique challenges and vibrant worlds. Experience Kirby's ability to transform and copy his enemies' powers, adding a strategic twist to the gameplay. With its charming visuals and engaging gameplay, Kirby Super Star offers an enjoyable experience for players of all ages.",
+    imgThumbSrc: kirbySuperStarThumb,
+    imgFullSrc: kirbySuperStarFull
+  },
+  {
+    id: 87532019,
+    title: 'The Legend of Zelda: A Link to the Past',
+    price: '$44.99',
+    priceReal: 44.99,
+    description:
+      'Embark on an unforgettable action-adventure with The Legend of Zelda: A Link to the Past. Dive into a rich fantasy world filled with secrets, puzzles, and epic battles. Unleash the power of the Master Sword as you journey between light and dark worlds, facing off against dangerous creatures and challenging dungeons. With its captivating storyline and timeless gameplay, A Link to the Past remains a true masterpiece in gaming history.',
+    imgThumbSrc: linkToThePastThumb,
+    imgFullSrc: linkToThePastFull
+  },
+  {
+    id: 30981725,
+    title: 'Mega Man X',
+    price: '$36.99',
+    priceReal: 36.99,
+    description:
+      'Take control of the legendary Mega Man X and blast your way through thrilling platforming levels. Traverse futuristic landscapes, defeat powerful bosses, and acquire new abilities to become the ultimate Maverick Hunter. With its fast-paced gameplay, stunning visuals, and iconic characters, Mega Man X offers an exhilarating experience for fans of the action genre.',
+    imgThumbSrc: megaManXThumb,
+    imgFullSrc: megaManXFull
+  },
+  {
+    id: 64258301,
+    title: 'Secret of Mana',
+    price: '$32.99',
+    priceReal: 32.99,
+    description:
+      'Embark on an epic quest in Secret of Mana, a classic action RPG. Join a young hero and his companions as they journey across a magical world to prevent an ancient evil from resurrecting. Experience real-time combat, upgrade your weapons, and harness the power of Mana in this beloved title. With its charming visuals, memorable soundtrack, and engaging gameplay, Secret of Mana is a must-play for RPG enthusiasts.',
+    imgThumbSrc: secretOfManaThumb,
+    imgFullSrc: secretOfManaFull
+  },
+  {
+    id: 42671095,
+    title: 'Star Fox',
+    price: '$37.99',
+    priceReal: 37.99,
+    description:
+      'Join Fox McCloud and his team of elite pilots in Star Fox, a thrilling space shooter. Pilot your Arwing spacecraft, engage in high-speed aerial combat, and battle against the forces of Andross. With its innovative gameplay, stunning visuals, and memorable characters, Star Fox delivers an adrenaline-pumping experience that will keep you on the edge of your seat.',
+    imgThumbSrc: starFoxThumb,
+    imgFullSrc: starFoxFull
+  },
+  {
+    id: 15983742,
+    title: 'Street Fighter II',
+    price: '$31.99',
+    priceReal: 31.99,
+    description:
+      'Step into the world of competitive fighting with Street Fighter II. Choose your fighter from a diverse roster of characters and engage in intense battles against friends or challenging AI opponents. Master intricate combos, unleash powerful special moves, and become a true street fighting champion. With its iconic characters, strategic gameplay, and memorable soundtrack, Street Fighter II is a legendary fighting game that has stood the test of time.',
+    imgThumbSrc: streetFighterIIThumb,
+    imgFullSrc: streetFighterIIFull
+  },
+  {
+    id: 76329580,
+    title: 'Super Bomberman',
+    price: '$28.99',
+    priceReal: 28.99,
+    description:
+      'Gather your friends and engage in explosive multiplayer mayhem with Super Bomberman. Navigate through labyrinthine levels, strategically place bombs, and outwit your opponents to be the last one standing. With its addictive multiplayer gameplay, colorful visuals, and chaotic action, Super Bomberman guarantees hours of fun-filled entertainment.',
+    imgThumbSrc: superBombermanThumb,
+    imgFullSrc: superBombermanFull
+  },
+  {
+    id: 82461937,
+    title: 'Super Mario Kart',
+    price: '$33.99',
+    priceReal: 33.99,
+    description:
+      'Get behind the wheel and race to victory in Super Mario Kart. Choose from a variety of beloved Mario characters, hop into your kart, and compete in exhilarating races across colorful tracks. Use power-ups, avoid hazards, and unleash your racing skills to claim the championship. With its iconic gameplay, tight controls, and unforgettable multiplayer battles, Super Mario Kart is a racing game that never goes out of style.',
+    imgThumbSrc: superMarioKartThumb,
+    imgFullSrc: superMarioKartFull
+  },
+  {
+    id: 57203468,
+    title: 'Super Metroid',
+    price: '$38.99',
+    priceReal: 38.99,
+    description:
+      'Experience the atmospheric exploration and intense action of Super Metroid. As intergalactic bounty hunter Samus Aran, delve into a vast and dangerous alien world, uncover hidden secrets, and confront formidable adversaries. With its non-linear gameplay, immersive atmosphere, and groundbreaking mechanics, Super Metroid has become a defining title in the metroidvania genre.',
+    imgThumbSrc: superMetroidThumb,
+    imgFullSrc: superMetroidFull
+  },
+  {
+    id: 41985627,
+    title: 'Super Mario World',
+    price: '$35.99',
+    priceReal: 35.99,
+    description:
+      'Join Mario on a classic platforming adventure in Super Mario World. Journey through the colorful and imaginative Dinosaur Land, defeat the Koopalings, and rescue Princess Peach. Discover hidden secrets, unlock power-ups, and explore sprawling levels in this iconic entry of the Super Mario series. With its tight controls, memorable soundtrack, and charming visuals, Super Mario World remains a beloved game for players of all ages.',
+    imgThumbSrc: superMarioWorldThumb,
+    imgFullSrc: superMarioWorldFull
+  }
+];
+
+export { defaultGame, products };
